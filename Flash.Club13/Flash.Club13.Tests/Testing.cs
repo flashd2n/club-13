e0f73d0b5ce6815223c8ea28062b6846b172a9a8
flash <k.kostov.eu@gmail.com>
2017-10-11 12:00:20 +0300

    setup tests

---
+++ Flash.Club13/Flash.Club13.Tests/Testing.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Flash.Club13.Tests
+{
+    [TestFixture]
+    public class Testing
+    {
+        [Test]
+        public void TestingTestFunctionality()
+        {
+            var a = 42;
+            var b = 72;
+
+            Assert.AreNotEqual(a, b);
+        }
+    }
+}
