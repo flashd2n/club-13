a98911b5a09082a8e974234ff21035171efdea1a
flash <k.kostov.eu@gmail.com>
2017-10-12 15:43:24 +0300

    unit and integration testing setup

---
+++ Flash.Club13/Flash.Club13.IntegrationTests/TestingIntegration.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flash.Club13.IntegrationTests
+{
+    [TestFixture]
+    public class TestingIntegration
+    {
+        [Test]
+        public void TestingTestFunctionality()
+        {
+            var a = 42;
+            var b = 72;
+
+            Assert.AreNotEqual(a, b);
+        }
+    }
+}
